6dde28b7d577b8e94a89894fdaf76bf7a4c275b4
bhvrgav <bhargav.venugopal@epfl.ch>
2022-06-03 00:54:44 +0200

    Added Basic Functionality for the various cuisines
    - added click listeners
    - shows name of cuisine on click

--- milestone-3/scripts/dropdown.js
+++ milestone-3/scripts/dropdown.js
@@ -1,3 +1,4 @@
+//Add click listener for dropDown button
 document.addEventListener('click', e =>{
     const isDropDownBtn = e.target.matches("[data-dropdown-button]")
     if (!isDropDownBtn && e.target.closest("[data-dropdown]") != null) return
@@ -7,23 +8,42 @@ document.addEventListener('click', e =>{
         currentDropDown = e.target.closest("[data-dropdown]")
         currentDropDown.classList.toggle("active")
     }
-})
-d3.csv("./milestone-3/data/cuisine_aggregates.csv").then( function(data) {
 
+})
+
+d3.csv("./milestone-3/data/cuisine_aggregates.csv").then( function(data) {
+    //Extract cuisines from csv
     var cuisines = d3.map(data, function(d){return(d.cuisine)})
     console.log(cuisines)
-    let cuisineBtn = document.getElementById('dropDownBtn')
+    let dropDownBtn = document.getElementById('dropDownBtn')
     let grid = document.getElementById("dropDownGrid")
 
-    cuisineBtn.addEventListener('click',e =>{
+    
+    dropDownBtn.addEventListener('click',() =>{
+        //Dynamically add cuisine names as button elements
         let options = cuisines.map(cuisine => '<div><button class="cuisine-link">'+cuisine+'</button></div>').join('\n')
-
         grid.innerHTML = options; 
-    })
-    cuisineBtn.addEventListener('hover',e =>{
-        let options = cuisines.map(cuisine => '<div><button class="cuisine-link">'+cuisine+'</button></div>').join('\n')
+        
+        //Add ClickListeners for newly added cuisine buttons
+        let btns = document.getElementsByClassName("cuisine-link")
+        console.log(btns)
+        for(const btn of btns){
+            btn.addEventListener('click', function (){
+                let dishes = document.getElementById("dishes")
+                const tag = document.createElement("div")
+                const text = document.createTextNode(btn.textContent)
 
-        grid.innerHTML = options; 
+                //Clear the children of dishes 
+                dishes.innerHTML = ""
+
+                //Close the dropdown menu
+                dropDownBtn.click()
+
+                //Append new child element to dishes
+                tag.appendChild(text)
+                dishes.appendChild(tag)
+            })
+       }
     })
 })
 
