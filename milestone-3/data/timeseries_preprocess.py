5d4ad1d40d3c5bb3ba177afac527112bdf3f403e
andreasaarrestad@gmail.com <andreasaarrestad@gmail.com>
2022-06-02 12:38:50 +0200

    utkast timeglass

---
+++ milestone-3/data/timeseries_preprocess.py
@@ -0,0 +1,23 @@
+from cmath import sin
+import pandas as pd
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+df = pd.read_csv('./milestone-3/data/cuisine_timeseries.csv')
+print(df)
+cuisines = df.columns.values.tolist()
+del cuisines[0]
+for i in range(1,1000):
+    variable = math.sin(0.01*i)
+    to_append = [str(i)]
+
+    for cuisine in cuisines:
+        to_append.append(df[cuisine].values[0] + variable)
+        
+    series = pd.Series(to_append, index = df.columns)
+    df = df.append(series, ignore_index=True)
+
+#print(df.head(10))    
+df.to_csv('./milestone-3/data/out.csv', index=False)
--- milestone-3/scripts/hourglass.js
+++ milestone-3/scripts/hourglass.js
@@ -1,167 +1,90 @@
 // set the dimensions and margins of the graph
-const margin = {top: 60, right: 125, bottom: 50, left: 350},
-    width = 1260 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+var margin2 = {top: 20, right: 300, bottom: 30, left: 155},
+    width2 = 1460 - margin2.left - margin2.right,
+    height2 = 400 - margin2.top - margin2.bottom;
 
 // append the svg object to the body of the page
-const svg2 = d3.select("#hourglass")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",`translate(${margin.left}, ${margin.top})`
-);
+var svg2 = d3.select("#hourglass")
+  .append("svg")
+    .attr("width", width2 + margin2.left + margin2.right)
+    .attr("height", height2 + margin2.top + margin2.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin2.left + "," + margin2.top + ")");
+
+var cuisine_timeseries = d3.csv("./milestone-3/data/out.csv")
 
 // Parse the Data
-d3.csv("./milestone-3/data/cuisine_avg.csv").then( function(data) {
+Promise.all([cuisine_timeseries]).then(function([data]){ 
 
-    // List of groups = header of the csv files
-    const keys = data.columns.slice(1)
+  // List of groups = header of the csv files
+  var keys = data.columns.slice(1)
 
-    // color palette
-    const color = d3.scaleOrdinal()
-        .domain(keys)
-        .range(d3.schemeSet2);
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain(d3.extent(data, function(d) { return d.x; }))
+    .range([ 0, width2 ]);
+  svg2.append("g")
+    .attr("transform", "translate(0," + height2 + ")")
+    .call(d3.axisBottom(x).ticks(5));
 
-    //stack the data?
-    const stackedData = d3.stack()
-        .keys(keys)
-        (data)
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 4000])
+    .range([ height2, 0 ]);
+  svg2.append("g")
+    .call(d3.axisLeft(y));
 
-    // Add X axis
-    const x = d3.scaleLinear()
-        .domain(d3.extent(data, function(d) { return d.year; }))
-        .range([ 0, width ]);
-    
-    const xAxis = svg2.append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x).ticks(5))
+  // color palette
+  var color = d3.scaleOrdinal()
+    .domain(keys)
+    .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf'])
 
+  //stack the data?
+  var stackedData = d3.stack()
+    .keys(keys)
+    (data)
+    //console.log("This is the stack result: ", stackedData)
 
-    // Add Y axis
-    const y = d3.scaleLinear()
-        .domain([0, 12000])
-        .range([ height, 0 ]);
-
-    svg2.append("g")
-        .call(d3.axisLeft(y).ticks(5))
-
-
-
-    //////////
-    // BRUSHING AND CHART //
-    //////////
-
-    // Add a clipPath: everything out of this area won't be drawn.
-    const clip = svg2.append("defs").append("svg:clipPath")
-        .attr("id", "clip")
-        .append("svg:rect")
-        .attr("width", width )
-        .attr("height", height )
-        .attr("x", 0)
-        .attr("y", 0);
-
-    // Add brushing
-    const brush = d3.brushX()                 // Add the brush feature using the d3.brush function
-        .extent( [ [0,0], [width,height] ] ) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
-        .on("end", updateChart) // Each time the brush selection changes, trigger the 'updateChart' function
-
-    // Create the scatter variable: where both the circles and the brush take place
-    const areaChart = svg2.append('g')
-        .attr("clip-path", "url(#clip)")
-
-    // Area generator
-    const area = d3.area()
-        .x(function(d) { return x(d.data.year); })
+  // Show the areas
+  svg2
+    .selectAll("mylayers")
+    .data(stackedData)
+    .enter()
+    .append("path")
+      .style("fill", function(d) { console.log(d.key) ; return color(d.key); })
+      .attr("d", d3.area()
+        .x(function(d, i) { return x(d.data.x); })
         .y0(function(d) { return y(d[0]); })
         .y1(function(d) { return y(d[1]); })
-
-    // Show the areas
-    areaChart.selectAll("mylayers")
-        .data(stackedData)
-        .join("path")
-        .attr("class", function(d) { return "myArea " + d.key })
-        .style("fill", function(d) { return color(d.key); })
-        .attr("d", area)
-
-    // Add the brushing
-    areaChart.append("g")
-        .attr("class", "brush")
-        .call(brush);
-
-    let idleTimeout
-    function idled() { idleTimeout = null; }
-
-    // A function that update the chart for given boundaries
-    function updateChart(event,d) {
-        extent = event.selection
-
-        // If no selection, back to initial coordinate. Otherwise, update X axis domain
-        if(!extent){
-            if (!idleTimeout) return idleTimeout = setTimeout(idled, 350); // This allows to wait a little bit
-            x.domain(d3.extent(data, function(d) { return d.year; }))
-        }else{
-            x.domain([ x.invert(extent[0]), x.invert(extent[1]) ])
-            areaChart.select(".brush").call(brush.move, null) // This remove the grey brush area as soon as the selection has been done
-        }
-
-        // Update axis and area position
-        xAxis.transition().duration(1000).call(d3.axisBottom(x).ticks(5))
-        areaChart.selectAll("path")
-            .transition().duration(1000)
-            .attr("d", area)
-    }
-
-
-
-    //////////
-    // HIGHLIGHT GROUP //
-    //////////
-
-    // What to do when one group is hovered
-    const highlight = function(event,d){
-        // reduce opacity of all groups
-        d3.selectAll(".myArea").style("opacity", .1)
-        // expect the one that is hovered
-        d3.select("."+d).style("opacity", 1)
-    }
-
-    // And when it is not hovered anymore
-    const noHighlight = function(event,d){
-        d3.selectAll(".myArea").style("opacity", 1)
-    }
-
-
-
-    //////////
-    // LEGEND //
-    //////////
+    )
 
     // Add one dot in the legend for each name.
     const size = 20
     svg2.selectAll("myrect")
         .data(keys)
         .join("rect")
-        .attr("x", 800)
+        .attr("x", 1100)
         .attr("y", function(d,i){ return 10 + i*(size+5)}) // 100 is where the first dot appears. 25 is the distance between dots
         .attr("width", size)
         .attr("height", size)
         .style("fill", function(d){ return color(d)})
-        .on("mouseover", highlight)
-        .on("mouseleave", noHighlight)
+
 
     // Add one dot in the legend for each name.
     svg2.selectAll("mylabels")
         .data(keys)
         .join("text")
-        .attr("x", 800 + size*1.2)
+        .attr("x", 1100 + size*1.2)
         .attr("y", function(d,i){ return 10 + i*(size+5) + (size/2)}) // 100 is where the first dot appears. 25 is the distance between dots
         .style("fill", function(d){ return color(d)})
         .text(function(d){ return d})
         .attr("text-anchor", "left")
         .style("alignment-baseline", "middle")
-        .on("mouseover", highlight)
-        .on("mouseleave", noHighlight)
+
+
+    
 
 })
+
 
