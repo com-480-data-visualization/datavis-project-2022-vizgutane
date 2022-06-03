0d3f015a302a2029e7503fae90ba2ce405999dba
andreasaarrestad@gmail.com <andreasaarrestad@gmail.com>
2022-06-03 10:43:04 +0200

    timeseries preprocess update

--- milestone-3/data/timeseries_preprocess.py
+++ milestone-3/data/timeseries_preprocess.py
@@ -6,7 +6,6 @@ import numpy as np
 import math
 
 df = pd.read_csv('./milestone-3/data/cuisine_timeseries.csv')
-print(df)
 cuisines = df.columns.values.tolist()
 del cuisines[0]
 for i in range(1,1000):
@@ -14,7 +13,7 @@ for i in range(1,1000):
     to_append = [str(i)]
 
     for cuisine in cuisines:
-        to_append.append(df[cuisine].values[0] + variable)
+        to_append.append(df[cuisine].values[0] + variable*df[cuisine].values[0]*0.1)
         
     series = pd.Series(to_append, index = df.columns)
     df = df.append(series, ignore_index=True)
